274cb7a4a64e118341002db572371d788f0dd7ad
Alper Duzgun <alper-mf@hotmail.com>
2022-06-27 02:28:29 +0300

    MAIN-IOS Splash And App Review Video Is Added

--- lib/screen/landing/splash_screen/view/splash_view.dart
+++ lib/screen/landing/splash_screen/view/splash_view.dart
@@ -1,3 +1,6 @@
+import 'package:dop_flutter_base_project/app/constants/assets/assets_image.dart';
+import 'package:dop_flutter_base_project/app/navigation/size_config.dart';
+import 'package:dop_flutter_base_project/app/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,6 +13,22 @@ class Splash extends GetView<SplashViewModel> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: controller.scaffoldKey,
+      backgroundColor: AppColor.marvelRed,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AspectRatio(
+              aspectRatio:
+                  // genişlik/yükseklik
+                  SizeConfig.width * 8 / SizeConfig.height * 0.5,
+              child: Image.asset(AssetsImage.marvelLogo),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
